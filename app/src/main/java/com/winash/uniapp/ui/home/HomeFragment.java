3be701ddedc67f3630eb7d6cfe7555c21d05c82e
phaniteja18 <87517714+gvsphaniteja@users.noreply.github.com>
2022-05-14 12:31:48 +0530

    Committed

--- app/src/main/java/com/winash/uniapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/winash/uniapp/ui/home/HomeFragment.java
@@ -1,37 +1,27 @@
 package com.winash.uniapp.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-<<<<<<< Updated upstream
 import com.google.firebase.auth.FirebaseAuth;
-import com.winash.uniapp.LoginUser;
-=======
->>>>>>> Stashed changes
 import com.winash.uniapp.R;
 import com.winash.uniapp.databinding.FragmentHomeBinding;
-import com.winash.uniapp.navigationui;
 
 public class HomeFragment extends Fragment {
-<<<<<<< Updated upstream
 TextView simply;
 FirebaseAuth fauth;
-=======
 
 
 private FragmentHomeBinding binding;
 
->>>>>>> Stashed changes
+
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         fauth=FirebaseAuth.getInstance();
