b42927fa34cb6217a4a4e7f8b6c127948ad1731c
Ilamparithi <73892419+Ilamparithi-070@users.noreply.github.com>
2022-05-16 13:51:23 +0530

    Unit Testing

--- app/src/test/java/com/winash/uniapp/ApplicantTest.java
+++ app/src/test/java/com/winash/uniapp/ApplicantTest.java
@@ -3,29 +3,52 @@ package com.winash.uniapp;
 import junit.framework.TestCase;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class ApplicantTest extends TestCase {
     Applicant applicant=new Applicant("Abc","Def","AbcDef@gmail.com","123456789",true,"A123");
+    @Test
     public void testGetFname() {
         Assert.assertEquals("Abc",applicant.getFname());
     }
+    @Test
+    public void testNotGetFname() {
+        Assert.assertNotEquals("abc",applicant.getFname());
+    }
+    @Test
     public void testIsBlack() {
         Assert.assertTrue(applicant.isBlack());
     }
-
+    @Test
+    public void testNotIsBlack() {
+        Assert.assertFalse(!applicant.isBlack());
+    }
+    @Test
     public void testGetLname() {
         assertEquals("Def",applicant.getLname());
     }
-
+    @Test
+    public void testNotGetLname() {
+        Assert.assertNotEquals("def",applicant.getLname());
+    }
+    @Test
     public void testGetEmail() {
         assertEquals("AbcDef@gmail.com",applicant.getEmail());
     }
-
+    @Test
     public void testGetPhone() {
         assertEquals("123456789",applicant.getPhone());
     }
-
+    @Test
+    public void testNotGetPhone() {
+        Assert.assertNotEquals("987654321",applicant.getPhone());
+    }
+    @Test
     public void testGetApplicantid() {
         assertEquals("A123",applicant.getApplicantid());
+    }
+    @Test
+    public void testNotGetApplicantid() {
+        Assert.assertNotEquals("A_123",applicant.getApplicantid());
     }
 }
--- app/src/test/java/com/winash/uniapp/ui/FAQ/FAQ_questionTest.java
+++ app/src/test/java/com/winash/uniapp/ui/FAQ/FAQ_questionTest.java
@@ -2,8 +2,12 @@ package com.winash.uniapp.ui.FAQ;
 
 import junit.framework.TestCase;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FAQ_questionTest extends TestCase {
     FAQ_question faq = new FAQ_question();
+    @Test
     public void testGetQuestion() {
         //Test whether the initialization is set to NULL
         assertNull(faq.getQuestion());
@@ -11,7 +15,12 @@ public class FAQ_questionTest extends TestCase {
         faq.question="What to do?";
         assertEquals("What to do?",faq.getQuestion());
     }
-
+    @Test
+    public void testNotGetQuestion() {
+        faq.question="What to do?";
+        Assert.assertNotEquals("what to do?",faq.getQuestion());
+    }
+    @Test
     public void testGetAnswer() {
         //Test whether the initialization is set to NULL
         assertNull(faq.getAnswer());
@@ -19,12 +28,16 @@ public class FAQ_questionTest extends TestCase {
         faq.answer="Have fun!";
         assertEquals("Have fun!",faq.getAnswer());
     }
-
+    @Test
     public void testIsExpandable() {
         //Test whether the initialized value is set to false
         assertFalse(faq.isExpandable());
     }
-
+    @Test
+    public void testNotIsExpandable() {
+        assertTrue(!faq.isExpandable());
+    }
+    @Test
     public void testSetExpandable() {
         //Update value and then test
         faq.setExpandable(true);
